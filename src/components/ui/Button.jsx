eada09ba8c41fe6c9c01e0b4826011647cc936e9
alliglobal <138554195+alliglobal@users.noreply.github.com>
2024-09-13 10:08:06 +0700

    update link register

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -7,7 +7,7 @@ export default function Button({
   onClick = () => {},
 }) {
   return (
-    <a href="https://cointhucchien.com">
+    <a href="https://forms.gle/WQGbqLwmxicfMwcp8">
     <button
       onClick={onClick}
       className={`bg-gradient-lr flex items-center gap-2 rounded-full px-7 py-4 text-lg font-semibold text-white transition hover:-rotate-3 ${className}`}
